ee7f5d9e6d799ade7d2f6a8a3d454046929d97c5
Victor Motta <victormotta7@gmail.com>
2022-07-30 00:02:51 -0300

    fix table error

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -6,93 +6,97 @@ const Cart = () => {
     <div className={styles.container}>
       <div className={styles.left}>
         <table className={styles.table}>
-          <tr className={styles.tr}>
-            <th>Product</th>
-            <th>Name</th>
-            <th>Extras</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-          </tr>
-          <tr>
-            <td className={styles.imgTd}>
-              <div className={styles.imgContainer}>
-                <Image src="/images/pizza.png" layout="fill" objectFit="cover" alt="" />
-              </div>
-            </td>
-            <td>
-              <span className={styles.name}>CARALZO</span>
-            </td>
-            <td>
-              <span className={styles.extras}>Double ingredients, spicy sauce</span>
-            </td>
-            <td>
-              <span className={styles.price}>$19.90</span>
-            </td>
-            <td>
-              <span className={styles.quantity}>2</span>
-            </td>
-            <td>
-              <span className={styles.total}>$39.80</span>
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.imgTd}>
-              <div className={styles.imgContainer}>
-                <Image src="/images/pizza.png" layout="fill" objectFit="cover" alt="" />
+          <thead>
+            <tr className={styles.tr}>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Extras</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+          </thead>
 
-              </div>
-            </td>
-            <td>
-              <span className={styles.name}>CARALZO</span>
-            </td>
-            <td>
-              <span className={styles.extras}>Double ingredients, spicy sauce</span>
-            </td>
-            <td>
-              <span className={styles.price}>$19.90</span>
-            </td>
-            <td>
-              <span className={styles.quantity}>2</span>
-            </td>
-            <td>
-              <span className={styles.total}>$39.80</span>
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.imgTd}>
-              <div className={styles.imgContainer}>
-                <Image src="/images/pizza.png" layout="fill" objectFit="cover" alt="" />
-
-              </div>
-            </td>
-            <td>
-              <span className={styles.name}>CARALZO</span>
-            </td>
-            <td>
-              <span className={styles.extras}>Double ingredients, spicy sauce</span>
-            </td>
-            <td>
-              <span className={styles.price}>$19.90</span>
-            </td>
-            <td>
-              <span className={styles.quantity}>2</span>
-            </td>
-            <td>
-              <span className={styles.total}>$39.80</span>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className={styles.imgTd}>
+                <div className={styles.imgContainer}>
+                  <Image src="/images/pizza.png" layout="fill" objectFit="cover" alt="" />
+                </div>
+              </td>
+              <td>
+                <span className={styles.name}>CARALZO</span>
+              </td>
+              <td>
+                <span className={styles.extras}>Double ingredients, spicy sauce</span>
+              </td>
+              <td>
+                <span className={styles.price}>$19.90</span>
+              </td>
+              <td>
+                <span className={styles.quantity}>2</span>
+              </td>
+              <td>
+                <span className={styles.total}>$39.80</span>
+              </td>
+            </tr>
+            <tr>
+              <td className={styles.imgTd}>
+                <div className={styles.imgContainer}>
+                  <Image src="/images/pizza.png" layout="fill" objectFit="cover" alt="" />
+                </div>
+              </td>
+              <td>
+                <span className={styles.name}>CARALZO</span>
+              </td>
+              <td>
+                <span className={styles.extras}>Double ingredients, spicy sauce</span>
+              </td>
+              <td>
+                <span className={styles.price}>$19.90</span>
+              </td>
+              <td>
+                <span className={styles.quantity}>2</span>
+              </td>
+              <td>
+                <span className={styles.total}>$39.80</span>
+              </td>
+            </tr>
+            <tr>
+              <td className={styles.imgTd}>
+                <div className={styles.imgContainer}>
+                  <Image src="/images/pizza.png" layout="fill" objectFit="cover" alt="" />
+                </div>
+              </td>
+              <td>
+                <span className={styles.name}>CARALZO</span>
+              </td>
+              <td>
+                <span className={styles.extras}>Double ingredients, spicy sauce</span>
+              </td>
+              <td>
+                <span className={styles.price}>$19.90</span>
+              </td>
+              <td>
+                <span className={styles.quantity}>2</span>
+              </td>
+              <td>
+                <span className={styles.total}>$39.80</span>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
       <div className={styles.right}>
         <div className={styles.wrapper}>
           <h2 className={styles.title}>CART TOTAL</h2>
           <table className={styles.checkout}>
-            <tr><td>Subtotal:</td><td className={styles.value}>$79.80</td></tr>
-            <tr><td>Discount:</td><td className={styles.value}>$0.00</td></tr>
-            <tr><td>Total:</td><td className={styles.value}>$79.80</td></tr>
+            <tbody>
+              <tr><td>Subtotal:</td><td className={styles.value}>$79.80</td></tr>
+              <tr><td>Discount:</td><td className={styles.value}>$0.00</td></tr>
+              <tr><td>Total:</td><td className={styles.value}>$79.80</td></tr>
+            </tbody>
           </table>
-
           <button className={styles.button}>CHECKOUT NOW!</button>
         </div>
       </div>
---
+++ src/pages/orders/[id].jsx
@@ -0,0 +1,10 @@
+import styles from '../../styles/Order.module.scss'
+import Image from 'next/image'
+
+const Order = () => {
+  return (
+    <div className={styles.container}></div>
+  )
+}
+
+export default Order
