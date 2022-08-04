8331e04247148f6aeb773c911d96b2037f4cdb6d
Victor Motta <victormotta7@gmail.com>
2022-08-03 23:49:25 -0300

    admin dashboard

---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,117 @@
+import styles from "../../styles/Admin.module.scss"
+import Image from "next/image"
+import axios from "axios"
+import { useState } from "react"
+
+const Index = ({ orders, products }) => {
+  const [pizzaList, setPizzaList] = useState(products);
+  const [orderList, setOrderList] = useState(orders);
+  const status = ["preparing", "on the way", "", "delivered"]
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete("http://localhost:3000/api/products/" + id)
+      setPizzaList(pizzaList.filter((pizza) => pizza._id !== id))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleStatus = async (id) => {
+
+    const item = orderList.filter(order => order._id === id)[0]
+    const currentStatus = item.status
+
+    if (currentStatus < 2) {
+      try {
+        const res = await axios.put("http://localhost:3000/api/orders/" + id, { status: (currentStatus === 1) ? currentStatus + 2 : currentStatus + 1 })
+        setOrderList([res.data, ...orderList.filter(order => order._id !== id)])
+      } catch (err) {
+        console.log(err)
+      }
+      console.log(currentStatus);
+    } else {
+      try {
+        setOrderList(orderList.filter((order) => order._id !== id))
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Orders</h1>
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.trTitle}>
+              <th>Id</th>
+              <th>Customer</th>
+              <th>Total Price</th>
+              <th>Payment Method</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          {orderList.map((order) => (
+            <tbody key={order._id}>
+              <tr className={styles.trTitle}>
+                <td>{order._id}</td>
+                <td>{order.customer}</td>
+                <td>${order.total}.00</td>
+                <td>{order.method === 0 ? (<span>cash</span>) : (<span>paid</span>)}</td>
+                <td>{status[order.status]}</td>
+                <td>
+                  <button className={styles.button} onClick={() => handleStatus(order._id)}>{order.status < 2 ? "Next Stage" : "Remove Order"}</button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Products</h1>
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.trTitle}>
+              <th>Image</th>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          {pizzaList.map((product) => (
+            <tbody key={product._id}>
+              <tr className={styles.trTitle}>
+                <td><Image src={product.img} width={50} height={50} objectFit="cover" alt="" /></td>
+                <td>{product._id}</td>
+                <td>{product.title}</td>
+                <td>${product.prices[0]}.00</td>
+                <td>
+                  <button className={styles.button}>Edit</button>
+                  <button className={styles.button} onClick={() => handleDelete(product._id)}>Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const productRes = await axios.get("http://localhost:3000/api/products")
+  const orderRes = await axios.get("http://localhost:3000/api/orders")
+
+  return {
+    props: {
+      products: productRes.data,
+      orders: orderRes.data,
+    }
+  }
+}
+
+export default Index
--- src/pages/api/orders/[id].js
+++ src/pages/api/orders/[id].js
@@ -15,9 +15,25 @@ const handler = async (req, res) => {
       res.status(500).json(err);
     }
   }
+
   if (method === 'PUT') {
+    try {
+      const order = await Order.findByIdAndUpdate(id, req.body, {
+        new: true
+      });
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
+
   if (method === 'DELETE') {
+    try {
+      await Order.findByIdAndDelete(id);
+      res.status(200).json('The order has been delivered');
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 };
 
